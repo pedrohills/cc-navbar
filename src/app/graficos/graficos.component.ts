bd82b08f64c5d1f1d56188b0e6e29b342ad2756b
Pedro Henrique <gpedroemail@gmail.com>
2018-08-22 10:51:03 -0300

    Páginas mockadas

---
+++ src/app/graficos/graficos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'graficos',
+  templateUrl: './graficos.component.html',
+  styleUrls: ['./graficos.component.css']
+})
+export class GraficosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
